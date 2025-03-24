e37df5954ceb6e602b910da109d7d145c1d8838f
dhinakaran2425 <dhinakaran2425@gmail.com>
2025-03-24 15:47:37 +0530

    update in code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3001,
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://ds-chatapplication.onrender.com/",
         changeOrigin: true,
       },
     },
